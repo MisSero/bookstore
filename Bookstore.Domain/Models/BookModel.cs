071f41cb4359a41fb60f52dda39ec02be7dbab3d
MisSero <misha555835@gmail.com>
2023-08-25 13:13:34 +0300

    add BookModel, OrderModel

---
+++ Bookstore.Domain/Models/BookModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Bookstore.Domain.Models;
+
+public class BookModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Released { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ Bookstore.Domain/Models/OrderModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookstore.Domain.Models;
+
+public class OrderModel
+{
+    public int Number { get; set; }
+    public DateTime Created { get; set; }
+    public decimal Cost { get; set; }
+    public List<BookModel> Books { get; set; } = new();
+}
