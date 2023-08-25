24432b22a8d5a41e05f9ad57c6000a778e2ac572
MisSero <misha555835@gmail.com>
2023-08-25 13:13:54 +0300

    add BookMapper, OrderMapper

---
+++ Bookstore.Domain/Mappers/BookMapper.cs
@@ -0,0 +1,20 @@
+﻿using Bookstore.DAL.Entities;
+using Bookstore.Domain.Models;
+
+namespace Bookstore.Domain.Mappers;
+public static class BookMapper
+{
+    public static BookModel EntityToModel(Book book)
+    {
+        BookModel bookModel = new BookModel()
+        {
+            Id = book.Id,
+            Title = book.Title,
+            Author = book.Author,
+            Released = book.Released.ToShortDateString(),
+            Price = book.Price,
+        };
+
+        return bookModel;
+    }
+}
---
+++ Bookstore.Domain/Mappers/OrderMapper.cs
@@ -0,0 +1,26 @@
+﻿using Bookstore.DAL.Entities;
+using Bookstore.Domain.Models;
+
+namespace Bookstore.Domain.Mappers;
+
+public static class OrderMapper
+{
+    public static OrderModel EntityToModel(Order order)
+    {
+        List<BookModel> books = new ();
+        foreach (Book book in order.Books)
+        {
+            books.Add(BookMapper.EntityToModel(book));
+        }
+
+        OrderModel orderModel = new ()
+        {
+            Number = order.Id,
+            Created = order.Created,
+            Cost = order.Cost,
+            Books = books
+        };
+
+        return orderModel;
+    }
+}
